ca0167f8275e4f3da9b1957a71c5ce695515190b
William Dias Marcos <william.diasmarcos@gmail.com>
2022-08-15 20:27:00 -0300

    Part 02

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,71 +1,73 @@
 // react
 import { useState } from "react";
-
+// css
+import styles from "./Converter.module.css"
 
 function Converter (){
 
   const [text, setText] = useState("")
-
+  
   function onChange(event){
     setText(event.target.value)
   }
-
+  
   function upperCase(){
     setText(text.toUpperCase())
   }
-
+  
   function lowerCase(){
     setText(text.toLowerCase())
   }
-
+  
   function phraseCase(){
     setText(text[0].toUpperCase() + text.slice(1).toLowerCase())
   }
-
+  
   function titleCase(){
-    setText(text.toLowerCase().replace(/(?:^|\s)\S/g, 
+    setText(text.toLowerCase().replace(/(?:^|\s)\S/g,
     function(a){
-      return a.toUpperCase()
+    return a.toUpperCase()
     }))
   }
-
+  
   function alternatingCase(){
     let letters = text.toLowerCase().split("")
     for (let i = 0; i < letters.length; i = i + 2){
-      letters[i] = letters[i].toUpperCase()
+    letters[i] = letters[i].toUpperCase()
     }
     setText(letters.join(''))
   }
-
+  
   function reverse(){
     let lettersReverse = text.split("").reverse()
     setText(lettersReverse.join(''))
   }
-
+  
   async function copy(){
     await navigator.clipboard.writeText(text)
   }
-
-
+  
   function clear(){
     setText("")
   }
-  
+
   return(
     <>
-      <h1>Text Formatting</h1>
+      <h1 className={styles.title}>Text Formatting</h1>
       
-      <textarea name="text" id="text" cols="100" rows="10" value={text} onChange={onChange}></textarea>
-
-      <button onClick={upperCase}>MAIÚSCULAS</button>
-
-      <button onClick={lowerCase}>minúsculas</button>
-      <button onClick={phraseCase}>Frase</button>
-      <button onClick={titleCase}>Título</button>
-      <button onClick={alternatingCase}>AlTeRnAdAs</button>
-      <button onClick={reverse}>Reverter</button>
-      <button onClick={copy}>Copiar</button>
-      <button onClick={clear}>Limpar</button>
+      <div className={styles.area}>
+        <textarea className={styles.textArea} autoFocus name="text" id="text" cols="100" rows="10" value={text} onChange={onChange}></textarea>
+      </div>
+      <div className={styles.buttons}>
+        <button className={styles.button} onClick={upperCase}>MAIÚSCULAS</button>
+        <button className={styles.button} onClick={lowerCase}>minúsculas</button>
+        <button className={styles.button} onClick={phraseCase}>Frase</button>
+        <button className={styles.button} onClick={titleCase}>Título</button>
+        <button className={styles.button} onClick={alternatingCase}>AlTeRnAdAs</button>
+        <button className={styles.button} onClick={reverse}>Reverter</button>
+        <button className={styles.button} onClick={copy}>Copiar</button>
+        <button className={styles.button} onClick={clear}>Limpar</button>
+      </div>
     </>
   )
 }
